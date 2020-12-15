a56bdf7b4e2e92f1daf3cbeea3fa447d67133b58
George <989903+GeorgeLyon@users.noreply.github.com>
2020-12-15 09:53:20 -0800

    Revert "Support an older version of MLIR"
    This reverts commit a9bf10a0a64ca83a00c2a387c248a6f29a4cf918.

--- Sources/CMLIRStandard/CMLIRStandard.h
+++ Sources/CMLIRStandard/CMLIRStandard.h
@@ -1,5 +1,5 @@
 
 #include <mlir-c/StandardDialect.h>
 
-#include <mlir-c/StandardAttributes.h>
-#include <mlir-c/StandardTypes.h>
+#include <mlir-c/BuiltinAttributes.h>
+#include <mlir-c/BuiltinTypes.h>
