733972fd59253545448ecf97136ccac3178ffc21
George <989903+GeorgeLyon@users.noreply.github.com>
2021-01-18 14:04:30 -0800

    Remove unused code

--- Utilities/BoilerplateGenerator/Package.swift
+++ Utilities/BoilerplateGenerator/Package.swift
@@ -6,13 +6,9 @@ let package = Package(
   name: "BoilerplateGenerator",
   products: [
     .executable(name: "GenerateBlockInitializers", targets: ["GenerateBlockInitializers"]),
-    .executable(name: "GenerateOperationProtocolExtensions", targets: ["GenerateOperationProtocolExtensions"]),
-    .executable(name: "GenerateArrayStringsHelper", targets: ["GenerateArrayStringsHelper"]),
   ],
   targets: [
     .target(name: "Utilities"),
     .target(name: "GenerateBlockInitializers", dependencies: ["Utilities"]),
-    .target(name: "GenerateOperationProtocolExtensions", dependencies: ["Utilities"]),
-    .target(name: "GenerateArrayStringsHelper", dependencies: ["Utilities"]),
   ]
 )
