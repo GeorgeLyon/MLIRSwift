3c58d604203ad616ac221aa1eb043a249a2ce720
George <GeorgeLyon@users.noreply.github.com>
2021-02-02 13:11:35 -0800

    Add dictionary and array attributes

--- Sources/MLIR/Builtin Dialect/Attributes.swift
+++ Sources/MLIR/Builtin Dialect/Attributes.swift
@@ -5,6 +5,26 @@ extension Identifier {
   public static var type: Identifier { "type" }
 }
 
+extension Attribute: ExpressibleByArrayLiteral {
+  public init(arrayLiteral elements: MLIR.Attribute...) {
+    self = elements.withUnsafeBorrowedValues { buffer in
+      .borrow(mlirArrayAttrGet(MLIR.context, buffer.count, buffer.baseAddress))!
+    }
+  }
+}
+
+extension Attribute: ExpressibleByDictionaryLiteral {
+  public init(dictionaryLiteral elements: (MLIR.Identifier, MLIR.Attribute)...) {
+    self = elements
+      .map {
+        mlirNamedAttributeGet(.borrow($0.0), .borrow($0.1))
+      }
+      .withUnsafeBufferPointer { buffer in
+        .borrow(mlirDictionaryAttrGet(MLIR.context, buffer.count, buffer.baseAddress))!
+      }
+  }
+}
+
 extension Attribute {
   public static func type(_ type: MLIR.`Type`) -> Self {
     .borrow(mlirTypeAttrGet(.borrow(type)))!
@@ -16,3 +36,4 @@ extension Attribute {
     .borrow(value.withUnsafeMlirStringRef { mlirFlatSymbolRefAttrGet(MLIR.context, $0) })!
   }
 }
+
